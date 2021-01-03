9b26c2a5cbc4f9c0352c8d6caea3a633cd00677c
Marketa Sauerova <market.sauer@gmail.com>
2021-01-03 02:06:50 +0100

    Game loop and render method

--- src/com/mill/game/main/Game.java
+++ src/com/mill/game/main/Game.java
@@ -1,21 +1,86 @@
 package com.mill.game.main;
 
 import java.awt.*;
+import java.awt.image.BufferStrategy;
 
 public class Game extends Canvas implements Runnable {
     //16x9 ratio for the game window
     public static final int WIDTH = 640, HEIGHT = WIDTH / 12 * 9;
+    //main game thread
+    private Thread thread;
+    private boolean running = false;
 
     public Game(){
         new Window(WIDTH, HEIGHT, "Mill game", this);
     }
 
     public synchronized void start(){
+        thread = new Thread(this);
+        thread.start();
+        running = true;
+    }
+
+    public synchronized void stop(){
+        try{
+            thread.join();
+            running = false;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
 
+    /**
+     * Game loop
+     */
     public void run(){
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
+        long now;
+        while (running){
+            now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while (delta >= 1){
+                tick();
+                delta--;
+            }
+            if (running){
+                render();
+            }
+            frames++;
 
+            if (System.currentTimeMillis() - timer > 1000){
+                timer += 1000;
+                System.out.println("FPS: " + frames);
+                frames = 0;
+            }
+        }
+        stop();
+    }
+
+    private void tick(){
+
+    }
+
+    private void render(){
+        BufferStrategy bs = this.getBufferStrategy();
+        if (bs == null){
+            this.createBufferStrategy(3);
+            return;
+        }
+
+        Graphics g = bs.getDrawGraphics();
+
+        g.setColor(Color.blue);
+        g.fillRect(0,0, WIDTH, HEIGHT);
+
+        g.dispose();
+        bs.show();
     }
 
     public static void main(String args[]){
