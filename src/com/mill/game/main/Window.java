ee648fe0fcab552096ed228df2b76b13ae1ed469
Marketa Sauerova <market.sauer@gmail.com>
2021-03-25 14:45:22 +0100

    Add icon to the game

--- src/com/mill/game/main/Window.java
+++ src/com/mill/game/main/Window.java
@@ -1,14 +1,15 @@
 package com.mill.game.main;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 import java.awt.Canvas;
 import java.awt.Dimension;
 
 public class Window extends Canvas{
 
-
     public Window(int width, int height, String title, Game game) {
         JFrame frame = new JFrame(title);
+        ImageIcon img = new ImageIcon("./images/icon.png");
+        frame.setIconImage(img.getImage());
 
         frame.setPreferredSize(new Dimension(width, height));
         frame.setMaximumSize(new Dimension(width, height));
